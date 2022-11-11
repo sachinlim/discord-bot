fb042a80a840af642e8a88f86b8e68219ecac264
Sachin Limbu <sachin@sachins-mbp.home>
2022-11-11 18:59:10 +0000

    scraping to op.gg works through Selenium

--- in_game_scraper.py
+++ in_game_scraper.py
@@ -1,15 +1,33 @@
-# Returns sample data for now
-def return_lists():
-    blue_side = [['Jax', 'Diamond 3', '55%'],
-                 ['Hecarim', 'Diamond 2', '52%'],
-                 ['Viktor', 'Diamond 3', '48%'],
-                 ['Jhin', 'Diamond 4', '51%'],
-                 ['Rakan', 'Diamond 3', '54%']]
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-    red_side = [['Camille', 'Diamond 3', '53%'],
-                 ['Graves', 'Diamond 2', '49%'],
-                 ['Azir', 'Diamond 3', '50%'],
-                 ['Ashe', 'Diamond 4', '55%'],
-                 ['Miss Fortune', 'Diamond 3', '47%']]
 
-    return blue_side, red_side
+def get_data(summoner_name):
+    url = f'https://www.op.gg/summoners/euw/{summoner_name}/ingame'
+
+    path = 'chromedriver'
+    driver = webdriver.Chrome(path)
+    driver.get(url)
+
+    summoners = []
+    ranks = []
+    win_rates = []
+
+    summoner_name = driver.find_elements(By.XPATH, '//td[1]/a/div/img')
+    for summoner in summoner_name:
+        champ = summoner.get_attribute('alt')
+        summoners.append(champ)
+
+    current_rank = driver.find_elements(By.CLASS_NAME, 'current-rank')
+    for rank in current_rank:
+        formatted = rank.text.replace('(', ' (')
+        ranks.append(formatted)
+
+    winratio = driver.find_elements(By.XPATH, '//td[7]')
+    for ratios in winratio:
+        win_rates.append(ratios.text)
+
+    final_list = list(zip(summoners, ranks, win_rates))
+
+    driver.quit()
+    return final_list
