4499d5bca1686fbff1f4474c6208b228c929bdb4
Sachin Limbu <sachin@sachins-mbp.home>
2022-11-11 01:42:11 +0000

    adding a command to send embedded message for upcoming in-game scraping command

---
+++ in_game_scraper.py
@@ -0,0 +1,15 @@
+# Returns sample data for now
+def return_lists():
+    blue_side = [['Jax', 'Diamond 3', '55%'],
+                 ['Hecarim', 'Diamond 2', '52%'],
+                 ['Viktor', 'Diamond 3', '48%'],
+                 ['Jhin', 'Diamond 4', '51%'],
+                 ['Rakan', 'Diamond 3', '54%']]
+
+    red_side = [['Camille', 'Diamond 3', '53%'],
+                 ['Graves', 'Diamond 2', '49%'],
+                 ['Azir', 'Diamond 3', '50%'],
+                 ['Ashe', 'Diamond 4', '55%'],
+                 ['Miss Fortune', 'Diamond 3', '47%']]
+
+    return blue_side, red_side
