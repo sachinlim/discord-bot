2854cbe0c1de61bc9dcdc10d744728dab17a2a42
Sachin Limbu <sachin@sachins-mbp.home>
2022-11-11 19:00:32 +0000

    removed old code for !ig command

--- dukies-bot.py
+++ dukies-bot.py
@@ -109,50 +109,6 @@ async def ig(ctx, *, summoner_name):
 
 
 @bot.command()
-async def ig3(ctx, *, summoner_name):
-    formatted = passives.op_format(summoner_name)
-
-    embed = discord.Embed(
-        title= "Live Game Match-Up",
-        description= f"From {summoner_name}'s op.gg \n https://www.op.gg/summoners/euw/{formatted}/ingame",
-        colour= 0xbc8125
-    )
-
-    blue_side, red_side = in_game_scraper.return_lists()
-
-    blue_champs = blue_ranks = blue_wr = ""
-    red_champs = red_ranks = red_wr = ""
-
-    for items in blue_side:
-        blue_champs += items[0] + '\n'
-        blue_ranks += items[1] + '\n'
-        blue_wr += items[2] + '\n'
-
-    for items in red_side:
-        red_champs += items[0] + '\n'
-        red_ranks += items[1] + '\n'
-        red_wr += items[2] + '\n'
-
-    embed.add_field(name= "Blue Team", value= blue_champs, inline= True)
-    embed.add_field(name= "Rank", value= blue_ranks, inline= True)
-    embed.add_field(name= "Win Rate", value= blue_wr, inline= True)
-
-    embed.add_field(name= "Red Team", value= red_champs, inline= True)
-    embed.add_field(name= "Rank", value= red_ranks, inline= True)
-    embed.add_field(name= "Win Rate", value= red_wr, inline= True)
-
-    # finalise thumbnail icon
-    embed.set_thumbnail(url= 'https://static.wikia.nocookie.net/leagueoflegends/images/0/02/Season_2022_-_Challenger'
-                             '.png')
-    embed.timestamp = datetime.datetime.utcnow()
-    embed.set_footer(text= '\u200b',
-                     icon_url= 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/LoL_icon.svg/1200px'
-                               '-LoL_icon.svg.png')
-
-    await ctx.send(embed = embed)
-
-
-@bot.command()
 async def ig2(ctx, *, summoner_name):
     if " " in summoner_name:
         formatted = summoner_name.replace(" ", "%20")
