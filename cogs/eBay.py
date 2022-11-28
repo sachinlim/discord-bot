c7ba88c1575486749e9e8507dff5070fc353b04d
Sachin Limbu <sachin857@hotmail.com>
2022-11-28 14:54:38 +0000

    updating search filters comment

--- cogs/eBay.py
+++ cogs/eBay.py
@@ -12,7 +12,7 @@ class EbayScraper(commands.Cog):
         self.bot = bot
 
     def website_data(self, search):
-        # URL contains search filters: used items, sold listings, and UK only
+        # URL contains search filters: exact match any order, used items, sold listings, and UK only
         url = f'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw={search}' \
               f'&_in_kw=4&_ex_kw=&_sacat=0&LH_Sold=1&_udlo=&_udhi=&LH_ItemCondition=4&_samilow=&_samihi=&_stpos=M300AA' \
               f'&_sargn=-1%26saslc%3D1&_fsradio2=%26LH_LocatedIn%3D1&_salic=3&LH_SubLocation=1&_sop=12&_dmd=1&_ipg=60' \
